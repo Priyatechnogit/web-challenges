be9dc57119b420e1f10415fbfb2950350a1b5d65
Priya <pitlapriya4u@gmail.com>
2026-03-19 17:05:02 +0100

    js-conditions and booleans log type completed

---
+++ javascript/js-conditions-and-booleans_log-type/js/index.js
@@ -0,0 +1,39 @@
+console.clear();
+//const data = "String";
+const data = [1,2,3];
+switch (typeof data){
+    case "undefined":
+        console.log("undefined!");
+         break;
+    case "number":
+        if (Number.isNaN(data)){
+        console.log("not a number!");
+        } else {
+        console.log("number!");
+        }
+        break;
+    case "string":
+        console.log("string!");
+         break;
+    case "boolean":
+    console.log("boolean!");
+         break;
+         case "function":
+    console.log("function!");
+    break;
+
+  case "object":
+    if (data === null) {
+      console.log("null!");
+    } else if (Array.isArray(data)) {
+      console.log("array!");
+    } else {
+      console.log("object!");
+    }
+    break;
+
+  default:
+    console.log("I have no idea!");
+}
+
+   
