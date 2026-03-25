9134196d63c36263587abaa40ae096fbddbcfebd
Priya <pitlapriya4u@gmail.com>
2026-03-25 23:02:16 +0100

    js forms product teaser recreating task done

---
+++ javascript/jsforms/js-createelement_product-teaser/js/index.js
@@ -0,0 +1,38 @@
+console.clear();
+
+const name = "Queen angelfish";
+const description =
+  "With their bright colors and deep, laterally compressed bodies, marine angelfishes are some of the more conspicuous residents of the aquarium. The queen angelfish grows to be 45 cm. With neon blue and yellow scales and iridescent purple and orange markings, surprisingly it is not conspicuous, and actually hides very well, and is very shy.";
+const category1 = "Freshwater";
+const category2 = "Large aquarium";
+const category3 = "Plankton Diet";
+const price = "149,99 €";
+const imageSrc =
+  "https://unsplash.com/photos/3VOTHTrE614/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjU5NTM3NTA2&force=true&w=640";
+
+
+const newProduct = document.createElement("section");
+newProduct.innerHTML = `
+<article class="product">
+<img class="product__image" src="${imageSrc}" alt="${name}" />
+<h2 class="product__name">"${name}"</h2>
+
+<ul class="product__categories">
+ <li class="product__category">"${category1}"</li>
+ <li class="product__category">"${category2}"</li>
+ <li class="product__category">"${category3}"</li>
+ </ul>
+ <p class="product__description">"${description}"</p>
+ <footer class="product__footer">
+ <span class="product__price">${price}</span>
+
+    <button class="product__buy-button">Buy</button>
+  </article>
+`;
+
+document.body.append(newProduct);
+const buyButton = newProduct.querySelector(".product__buy-button");
+
+buyButton.addEventListener("click", () => {
+  console.log(`Product: ${name}, Price: ${price}`);
+});
