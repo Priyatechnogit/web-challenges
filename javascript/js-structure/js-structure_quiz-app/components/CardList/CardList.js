ea6e1674b3498fad3d03cf1e97bff8db320768be
Priya <pitlapriya4u@gmail.com>
2026-03-31 12:38:04 +0200

    js-structure quiz app is structured

---
+++ javascript/js-structure/js-structure_quiz-app/components/CardList/CardList.js
@@ -0,0 +1,15 @@
+import Card from "../Card/Card.js";
+import { cards } from "../DataCard/Cards.js";
+
+
+export default function CardList() {
+  const cardList = document.createElement("section");
+  cardList.classList.add("card-list");
+
+  cards.forEach((card) => {
+    const cardElement = Card(card);
+    cardList.append(cardElement);
+  });
+
+  return cardList;
+}
