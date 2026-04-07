5b958609722583e9431342ce8f83909f58af10a1
Priya <pitlapriya4u@gmail.com>
2026-04-07 12:44:52 +0200

    js-modern-syntax_back-to-basics added

---
+++ javascript/js-modern/js-modern-syntax_back-to-basics/js/index.js
@@ -0,0 +1,38 @@
+/*
+Now that you've practiced destructuring, default parameters, and the spread operator,
+try reversing the challenge by rewriting this code without using these modern features.
+*/
+
+export const getNameAndCountry = ({ name, country }) => [name, country];
+
+export const getRelocatedCity = (
+  city1,
+  city2 = { name: "Berlin", country: "Germany" }
+) => {
+  const [, country] = getNameAndCountry(city2);
+  return {
+    ...city1,
+    country,
+  };
+};
+
+
+/* export function getNameAndCountry(city) {
+  return [city.name,city.country];
+}
+
+export function getRelocatedCity(city1,city2){
+  if (city2 === undefined){
+    city2 = { name: "Berlin", country: "Germany" };
+  }
+  const temp = getNameAndCountry(city2);
+  const country = temp[1];
+ return {city1.name,city1.country}
+ */
+   
+
+
+
+}
+
+
