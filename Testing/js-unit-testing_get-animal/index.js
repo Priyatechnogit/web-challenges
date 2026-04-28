b0bb72a8174e9fa6a49d0c410e4fcd3972c6e6b9
Priya <pitlapriya4u@gmail.com>
2026-04-28 16:18:25 +0200

    unit testing task done

---
+++ Testing/js-unit-testing_get-animal/index.js
@@ -0,0 +1,12 @@
+export function getAnimal(animal) {
+   if (animal === "cats") {
+    return "I totally love cats!";
+  }
+
+  if (typeof animal === "string") {
+    return `I like ${animal}!`;
+  }
+
+  return "I do not like animals at all!";
+}
+
